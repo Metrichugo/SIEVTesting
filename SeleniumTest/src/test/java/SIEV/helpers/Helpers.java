7c1fe8c61d48a997bce98121a7cb2423e73b17a3
Victor Hugo Cardenas <metrichugo13@gmail.com>
2019-11-13 09:58:27 -0600

    Page Evaluacion fix errors

--- SeleniumTest/src/test/java/SIEV/helpers/Helpers.java
+++ SeleniumTest/src/test/java/SIEV/helpers/Helpers.java
@@ -1,5 +1,8 @@
 package SIEV.helpers;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Helpers {
 
 	public static final int defaultMillis = 500;
@@ -33,16 +36,40 @@ public class Helpers {
 	}
 	
 	public class EvaluacionPageHelpers{
-		public static final String FIRST_NAME_VALUE = "FABIANA";
-		public static final String SECOND_NAME_VALUE = "";
-		public static final String LASTNAME_VALUE = "MORALES";
-		public static final String SECOND_LASTNAME_VALUE = "DOMINGO";
-		public static final String REGION_VALUE = "R09";
+		/*Movimiento*/
+		public static final String REGION_VALUE = "R08";
 		public static final String CANAL_VALUE = "CACS";
-		public static final String FUERZA_VENTA_VALUE = "DISTRIBUIDOR DE PRUEBAS - PRUEBAS";
+		public static final String FUERZA_VENTA_VALUE = "CAC CAD  TAPACHULA I PZA CRISTAL - RACD115";
+		/*Tipo Movimiento*/
 		public static final String MOVIMIENTO_VALUE = "ACTIVACION";
 		public static final String PROYECTO_VALUE = "CLIENTE NACIONAL";
+		/*Tipo Evaluacion*/
 		public static final String TIPO_PLAZO_VALUE = "LIBRE";
 		public static final String FORMA_PAGO_VALUE = "DOMICILIACION";
+		/*Datos Personales*/
+		public static final String LASTNAME_VALUE = "MORALES";
+		public static final String SECOND_LASTNAME_VALUE = "DOMINGO";
+		public static final String FIRST_NAME_VALUE = "FABIANA";
+		public static final String SECOND_NAME_VALUE = "";
+		public static final String FEC_NAC_VALUE = "20/12/1985";
+		/*Domicilio Personal*/
+		public static final String CONTACTO_VALUE = "5512345678";
+		/*Información Crediticia*/
+		public static final String TARJETA_VALUE = "NO";
+		public static final String CREDITO_AUTO = "No tiene cuenta automotriz";
+		public static final String CREDITO_BANCO = "No tiene cuenta bancaria";
+	
+		
+		/*Helper Methods*/
+		public String getInvoicePattern() {
+			return "FIEV" + getYear() + REGION_VALUE.substring(2,3) + "[0-9]{6,7}".trim();
+		}
+		
+		public String getYear() {
+			Date date = new Date();
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			return Integer.toString(calendar.get(Calendar.YEAR));
+		}
 	}
 }
--- SeleniumTest/src/test/java/SIEV/pages/PageEvaluacion.java
+++ SeleniumTest/src/test/java/SIEV/pages/PageEvaluacion.java
@@ -1,26 +1,31 @@
 package SIEV.pages;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import SIEV.helpers.Helpers;
 
 public class PageEvaluacion {
 	private WebDriver driver;
-	private By regionTypeDrop;
-	private By canalTypeDrop;
-	private By fuerzaVTypeDrop;
-	private By movimientoTypeDrop;
-	private By proyectoTypeDrop;
-	private By tipoPlazoTypeDrop;
-	private By formaPagoTypeDrop;
-	private By lastNameField;
-	private By secondLastNameField;
-	private By firstNameField;
-	private By secondNameField;
-	private By calendarField;
-	private By rfcButton;
+	/*By's for Movimiento*/
+	private By regionTypeDrop,canalTypeDrop,fuerzaVTypeDrop;
+	/*By's for Tipo Movimiento*/
+	private By movimientoTypeDrop,proyectoTypeDrop;
+	/*By's for Evaluacion*/
+	private By tipoPlazoTypeDrop,formaPagoTypeDrop;
+	/*By's for Datos Personales*/
+	private By lastNameField,secondLastNameField,firstNameField,secondNameField,fecNacField,rfcButton,fillDomicilioButton;
+	/*By's for Domicilio*/
+	private By contactoField;
+	/*By's for Informacion Crediticia*/
+	private By tarjetaTypeDrop,creditoAutoTypeDrop,creditoBancarioTypeDrop;
+	/*By's for Autorizacion Evaluación*/
+	private By authButtonOne,authButtonTwo,evaluacionButton,firmaFisicaButton,confirmPreEvalButton,documentLink,confirmEvalButton,folioSIEVResult;
 	
 	public PageEvaluacion (WebDriver driver) {
 		this.driver = driver;
@@ -35,32 +40,101 @@ public class PageEvaluacion {
 		secondLastNameField = By.id("formSiev:apellidoMaterno");
 		firstNameField = By.id("formSiev:Pnombre");
 		secondNameField = By.id("formSiev:Snombre");
-		calendarField = By.id("formSiev:fechaNacimiento_input");
+		fecNacField = By.id("formSiev:fechaNacimiento_input");
 		rfcButton = By.id("formSiev:j_idt107");
+		authButtonOne = By.id("formSiev:blnAuto1");
+		authButtonTwo = By.id("formSiev:blnAuto2");
+		tarjetaTypeDrop = By.id("formSiev:cmbTc_input");
+		creditoAutoTypeDrop = By.id("formSiev:creditoAutomotriz_input");
+		creditoBancarioTypeDrop = By.id("formSiev:creditoBancario_input");
+		contactoField = By.id("formSiev:itNumContCli");
+		evaluacionButton = By.id("formSiev:btnEval");
+		fillDomicilioButton = By.id("formDlg:tblClientes:0:j_idt321");
+		firmaFisicaButton = By.id("formDlg:btnFisica");
+		confirmPreEvalButton = By.id("formDlg:btnSiPreEvalua");
+		documentLink = By.xpath("/html/body/center[2]/form[4]/div[6]/div[2]/a");
+		confirmEvalButton = By.id("formDlg:btnSiEvalua");
+		folioSIEVResult = By.id("formSiev:resultadoForm:j_idt199");
 	}
 
 	
 	public void assertEstructuraFolioSIEV() {
-		driver.findElement(firstNameField).sendKeys(Helpers.EvaluacionPageHelpers.FIRST_NAME_VALUE);
-		driver.findElement(secondNameField).sendKeys(Helpers.EvaluacionPageHelpers.SECOND_NAME_VALUE);
-		driver.findElement(lastNameField).sendKeys(Helpers.EvaluacionPageHelpers.LASTNAME_VALUE);
-		driver.findElement(secondLastNameField).sendKeys(Helpers.EvaluacionPageHelpers.SECOND_LASTNAME_VALUE);
+		setMovimiento();
+		setTipoMovimiento();
+		setEvaluacion();
+		setDatosPersonales();
+		setDomicilio();
+		setInformacionCrediticia();
+		autorizaEvaluacion();
+	}
+	
+	private void setMovimiento() {
 		Select region = new Select(driver.findElement(regionTypeDrop));
 		region.selectByVisibleText(Helpers.EvaluacionPageHelpers.REGION_VALUE);
-		Helpers.threadSleep(1);
+		Helpers.threadSleep(2);
 		Select canal = new Select(driver.findElement(canalTypeDrop));
 		canal.selectByVisibleText(Helpers.EvaluacionPageHelpers.CANAL_VALUE);
-		Helpers.threadSleepMillis(500);
+		Helpers.threadSleepMillis(800);
 		Select fuerzaVenta = new Select(driver.findElement(fuerzaVTypeDrop));
 		fuerzaVenta.selectByVisibleText(Helpers.EvaluacionPageHelpers.FUERZA_VENTA_VALUE);
+	}
+	
+	private void setTipoMovimiento() {
 		Select movimiento = new Select(driver.findElement(movimientoTypeDrop));
 		movimiento.selectByVisibleText(Helpers.EvaluacionPageHelpers.MOVIMIENTO_VALUE);
 		Select proyecto = new Select(driver.findElement(proyectoTypeDrop));
 		proyecto.selectByVisibleText(Helpers.EvaluacionPageHelpers.PROYECTO_VALUE);
+	}
+	
+	private void setEvaluacion() {
 		Select tipoPlazo = new Select(driver.findElement(tipoPlazoTypeDrop));
 		tipoPlazo.selectByVisibleText(Helpers.EvaluacionPageHelpers.TIPO_PLAZO_VALUE);
 		Select formaPago = new Select(driver.findElement(formaPagoTypeDrop));
 		formaPago.selectByVisibleText(Helpers.EvaluacionPageHelpers.FORMA_PAGO_VALUE);
+	}
+	
+	private void setDatosPersonales() {
+		driver.findElement(lastNameField).sendKeys(Helpers.EvaluacionPageHelpers.LASTNAME_VALUE);
+		driver.findElement(secondLastNameField).clear();
+		driver.findElement(secondLastNameField).sendKeys(Helpers.EvaluacionPageHelpers.SECOND_LASTNAME_VALUE);
+		driver.findElement(firstNameField).sendKeys(Helpers.EvaluacionPageHelpers.FIRST_NAME_VALUE);
+		driver.findElement(secondNameField).sendKeys(Helpers.EvaluacionPageHelpers.SECOND_NAME_VALUE);
+		driver.findElement(fecNacField).sendKeys(Helpers.EvaluacionPageHelpers.FEC_NAC_VALUE);
 		driver.findElement(rfcButton).click();
+		Helpers.threadSleep(2);
+		driver.findElement(fillDomicilioButton).click();	
+		Helpers.threadSleep(1);
+	}
+	
+	private void setDomicilio() {
+		driver.findElement(contactoField).sendKeys(Helpers.EvaluacionPageHelpers.CONTACTO_VALUE);
+	}
+	
+	private void setInformacionCrediticia() {
+		Select tarjeta = new Select(driver.findElement(tarjetaTypeDrop));
+		tarjeta.selectByVisibleText(Helpers.EvaluacionPageHelpers.TARJETA_VALUE);
+		Select creditoAuto = new Select(driver.findElement(creditoAutoTypeDrop));
+		creditoAuto.selectByVisibleText(Helpers.EvaluacionPageHelpers.CREDITO_AUTO);
+		Select creditoBanco = new Select(driver.findElement(creditoBancarioTypeDrop));
+		creditoBanco.selectByVisibleText(Helpers.EvaluacionPageHelpers.CREDITO_BANCO);
+	}
+	
+	private void autorizaEvaluacion() {
+		driver.findElement(authButtonOne).click();
+		driver.findElement(authButtonTwo).click();
+		Helpers.threadSleep(3);
+		driver.findElement(evaluacionButton).click();
+		Helpers.threadSleep(3);
+		driver.findElement(firmaFisicaButton).click();
+		Helpers.threadSleep(2);
+		driver.findElement(confirmPreEvalButton).click();
+		Helpers.threadSleep(2);
+		driver.findElement(documentLink).click();
+		Helpers.threadSleep(8);
+		driver.findElement(confirmEvalButton).click();
+		Helpers.threadSleep(10);
+		Pattern pattern = Pattern.compile(new Helpers().new EvaluacionPageHelpers().getInvoicePattern(), Pattern.MULTILINE);
+		Matcher matcher = pattern.matcher(driver.findElement(folioSIEVResult).getText());		
+		Assert.assertTrue(matcher.matches());
 	}
 }
